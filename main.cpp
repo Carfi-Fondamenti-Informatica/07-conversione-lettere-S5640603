c3c991824a6a536f3f0693248a21c4bd76600120
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-06 16:48:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "lib.cpp"
+#include "lib.h"
 using namespace std;
 
 int main() {
-    char c;
+    char c=0;
     cin>>c;
-    if (convertLet(c)==true) {
-        cout << c <<endl;
+    if(conv(c)==1){
+        cout<<c<<endl;
     }else{
-        cout<< "errore" <<endl;
+        cout<<"errore"<<endl;
     }
     return 0;
 }

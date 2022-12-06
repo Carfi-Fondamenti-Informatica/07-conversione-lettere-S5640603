349c42e4ece551cb32265b7af215fcb56b494e09
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-06 16:46:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,11 +1,11 @@
 #include "lib.h"
 
-bool convertLet(char c){
-    if (c>=65 && c<=90){
-        c+=32;
+bool conv(char &c){
+    if(c>=65 and c<=90){
+        c=c+32;
         return true;
-    }else if(c>=97 && c<=122){
-        c-=32;
+    }else if(c>=97 and c<=122){
+        c=c-32;
         return true;
     }else{
         return false;

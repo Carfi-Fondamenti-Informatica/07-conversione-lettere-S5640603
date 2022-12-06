2c9d0dc4e62d0f6997d7827d0a41ceca1c9257de
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-06 16:28:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+bool convertLet(char c){
+    if (c>=65 && c<=90){
+        c+=32;
+        return true;
+    }else if(c>=97 && c<=122){
+        c-=32;
+        return true;
+    }else{
+        return false;
+    }
+}

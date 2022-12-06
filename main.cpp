5fe14b5b19e7a5d3e09b024e2e8e2302582175dc
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-06 16:27:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.cpp"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char c;
+    cin>>c;
+    if (convertLet(c)==true) {
+        cout << c <<endl;
+    }else{
+        cout<< "errore" <<endl;
+    }
+    return 0;
 }
